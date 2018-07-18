14eaa2dcbde699d13e4a3b5c80b9888b65d83521
Hugo Wood <hx4.5@free.fr>
2018-07-18 13:24:59 +0200

    :recycle: simplify code by sending end-of-month starts reminder to a mailing list

--- functions/src/send-reminders.ts
+++ functions/src/send-reminders.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import * as firebase from "firebase-admin";
 import * as mailgun from "mailgun-js";
 
+const db = firebase.firestore();
 const config = functions.config();
 const mailgunClient = mailgun({
   domain: config.mailgun.domain,
@@ -27,60 +28,30 @@ export const sendEndOfMonthStartsReminder = functions.firestore
       month: "long"
     });
 
-    const latestImport = await firebase
-      .firestore()
-      .collection("employee-imports")
-      .orderBy("at", "desc")
-      .limit(1)
-      .get()
-      .then(result => result.docs[0]);
-    if (!latestImport) {
-      throw new Error("cannot find latest employee data import");
-    }
-
-    const allEmployeesQuerySnapshot = await latestImport.ref
-      .collection("employees")
-      .get();
-
-    const reminderSeriesRef = await firebase
-      .firestore()
-      .collection("end-of-month-starts-reminder-campaigns")
-      .add({
-        tick: tickSnapshot.ref,
-        size: allEmployeesQuerySnapshot.size
-      });
-
-    allEmployeesQuerySnapshot.forEach(async employeeSnapshot => {
-      const employee = employeeSnapshot.data();
-      if (!employee) {
-        console.warn(
-          `found no data for employee ${
-            employeeSnapshot.ref
-          } while iterating employees`
-        );
-        return;
-      }
-      const message = {
-        from: "Humeur du mois <humeur-du-mois@zenika.com>",
-        to: config.mailgun.recipientoverride || employee.email,
-        subject: `Humeur du mois is open for ${monthLongName}!`,
-        html: `
-          <p>Hi ${employee.fullName},</p>
-          <p>
-            Tell us how it's been for you this past month!
-            Go to https://humeur-du-mois.zenika.com.
-          </p>
-          <p>See you soon!</p>
+    const message = {
+      from: "Humeur du mois <humeur-du-mois@zenika.com>",
+      to: config.mailgun.recipientoverride || "all@zenika.com",
+      subject: `Humeur du mois is open for ${monthLongName}!`,
+      html: `
+        <p>Hi,</p>
+        <p>
+          Tell us how it's been for you this past month!
+          Go to https://humeur-du-mois.zenika.com.
+        </p>
+        <p>See you soon!</p>
         `
-      };
-      await firebase.firestore().runTransaction(async transaction => {
-        await mailgunClient.messages().send(message);
-        transaction.create(
-          reminderSeriesRef.collection("reminders").doc(employee.email),
-          {
-            emailSentAt: new Date().toISOString()
-          }
-        );
+    };
+
+    const reminderRef = await db
+      .collection("end-of-month-starts-reminders")
+      .add({
+        at: new Date().toISOString(),
+        tick: tickSnapshot.ref,
+        message
       });
+
+    await db.runTransaction(async transaction => {
+      await mailgunClient.messages().send(message);
+      transaction.update(reminderRef, { sentAt: new Date().toISOString() });
     });
   });
