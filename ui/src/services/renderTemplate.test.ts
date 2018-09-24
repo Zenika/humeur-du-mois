07a7dac9227cc19b75d4cc4b20f18f07009fab9f
Ked <kedketh@gmail.com>
2018-09-24 09:44:12 +0200

    Add some tests

--- ui/src/services/renderTemplate.test.ts
+++ ui/src/services/renderTemplate.test.ts
@@ -1,10 +1,12 @@
 import test from "ava";
 import { renderTemplate, StatsData, VoteData } from "./renderTemplate";
 
+var HtmlDiffer = require('html-differ').HtmlDiffer;
+
 test("renderTemplate test ok", t => {
   const statsData: StatsData = {
-    great: 2,
-    notThatGreat: 0,
+    great: 4,
+    notThatGreat: 1,
     notGreatAtAll: 0
   };
   let voteData: VoteData = [{
@@ -12,8 +14,27 @@ test("renderTemplate test ok", t => {
     counts: statsData,
     campaign_date: "September 2018"
   }];
+  let htmlDiffer = new HtmlDiffer();
+  const expectedResult = `
+  <table>
+    <tr>
+      <th>Campaign</th>
+      <th>😁</th><th>😐</th><th>😤</th>
+    </tr>
 
-  t.is(
+        <tr>
+          <td>September 2018</td>
+          <td>4</td><td>1</td><td>0</td>
+        </tr>
+  </table>
+`;
+const renderedHtml = renderTemplate(voteData)
+console.info(renderedHtml)
+console.info(expectedResult)
+  if(htmlDiffer.isEqual(renderedHtml, expectedResult))
+    t.pass();
+  else t.fail();
+  /*t.is(
     renderTemplate(voteData),`
       <table>
         <tr>
@@ -32,5 +53,5 @@ test("renderTemplate test ok", t => {
             </tr>
       </table>
     `
-  );
+  );*/
 });
