f2150a9b96a77aa73f9aca3e3c0a0f47242cdfca
Ked <kedketh@gmail.com>
2018-09-25 11:05:57 +0200

    Update firestore rules for new front end code

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -109,21 +109,14 @@ window.addEventListener("load", async function() {
       return;
     }
 
-    const latestImport = await db
-      .collection("employee-imports")
-      .orderBy("at", "desc")
-      .limit(1)
-      .get()
-      .then(result => result.docs[0]);
-    if (!latestImport) {
-      errorOut(new Error("cannot find latest employee data import"));
-      return;
-    }
-
-    const employeeSnapshot = await latestImport.ref
+    const employeeSnapshot = await db
       .collection("employees")
       .doc(userId)
       .get();
+    if (!employeeSnapshot) {
+      errorOut(new Error("cannot find latest employee data import"));
+      return;
+    }
     const employee = employeeSnapshot.data();
     if (!employee) {
       userEmail.innerText = userId;
