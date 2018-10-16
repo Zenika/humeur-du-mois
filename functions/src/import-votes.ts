c3f13c4cf0917900b9a58c71d75906b2744d0123
Ked <kedketh@gmail.com>
2018-10-16 13:59:09 +0200

    Fixed CircleCi import
    It still won't work until compute-stats and this branch are merged into master

--- functions/src/import-votes.ts
+++ functions/src/import-votes.ts
@@ -1,60 +1,18 @@
 import * as functions from "firebase-functions";
 import * as firebase from "firebase-admin";
-import { firestore } from "firebase-admin";
 
-import { Config, ImportVotesConfig } from "./config";
-import { Vote, castVote } from "./cast-vote";
-import { Timestamp } from "@google-cloud/firestore";
+import { Config } from "./config";
+import { Vote } from "./cast-vote";
 
 const config = functions.config() as Config;
 const importVotesConfigs = config.features.import_votes;
 const db = firebase.firestore();
 
-export const importVotes = functions.https.onRequest(
-  async (req: functions.Request, res: functions.Response) => {
-    if (!importVotesConfigs.enabled) {
-      console.info("Feature import votes disabled, aborting");
-      return;
-    }
-    const passedData = req.get("Authorization") || "";
-    const passedKey =
-      passedData.toString() === `Bearer ${importVotesConfigs.key}`;
-    if (!passedKey) {
-      console.info("Passed the wrong auth key, aborting");
-      return;
-    }
-    const passedJSONString = req.body.votesData;
-    const date: firestore.Timestamp = firestore.Timestamp.fromDate(new Date());
-    let validVotes: Vote[] = [
-      {
-        value: "",
-        campaign: "",
-        recordedAt: date,
-        fullName: "",
-        email: "",
-        managerEmail: "",
-        agency: ""
-      }
-    ];
-    try {
-      validVotes = fromJSONtoVote(passedJSONString);
-    } catch (e) {
-      console.error(e);
-    }
-
-    for (const validVote of validVotes) {
-      await db.collection("vote").add(validVote);
-    }
-  }
-);
-
 const fromJSONtoVote = (JSONString: string) => {
   const supposedlyValidVotes = JSON.parse(JSONString);
-  console.info(supposedlyValidVotes);
   const validVotes: Vote[] = [];
   for (const supposedlyValidVote of supposedlyValidVotes.votes) {
     if (
-      //supposedlyValidVote &&
       "value" in supposedlyValidVote &&
       "campaign" in supposedlyValidVote &&
       "recordedAt" in supposedlyValidVote &&
@@ -74,6 +32,47 @@ const fromJSONtoVote = (JSONString: string) => {
       });
     }
   }
-  console.info(validVotes);
   return validVotes;
 };
+
+export const importVotes = functions.https.onRequest(
+  async (req: functions.Request, res: functions.Response) => {
+    if (!importVotesConfigs.enabled) {
+      console.info("Feature import votes disabled, aborting");
+      return;
+    }
+    const passedData = req.get("Authorization") || "";
+    const passedKey =
+      passedData.toString() === `Bearer ${importVotesConfigs.key}`;
+    if (!passedKey) {
+      console.info("Passed the wrong auth key, aborting");
+      return;
+    }
+    const passedJSONString = req.body.votesData;
+    const date: firebase.firestore.Timestamp = firebase.firestore.Timestamp.fromDate(
+      new Date()
+    );
+    let validVotes: Vote[] = [
+      {
+        value: "",
+        campaign: "",
+        recordedAt: date,
+        fullName: "",
+        email: "",
+        managerEmail: "",
+        agency: ""
+      }
+    ];
+    try {
+      validVotes = fromJSONtoVote(passedJSONString);
+    } catch (e) {
+      console.error(e);
+      res.sendStatus(500);
+    }
+
+    for (const validVote of validVotes) {
+      await db.collection("vote").add(validVote);
+    }
+    res.sendStatus(200);
+  }
+);
