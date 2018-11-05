229be6afe91846b79c67561753b51058fd86cf9e
Hugo Wood <hx4.5@free.fr>
2018-11-05 15:48:28 +0100

    extract requeuing

--- functions/src/process-email-queue.ts
+++ functions/src/process-email-queue.ts
@@ -4,6 +4,7 @@ import { DocumentSnapshot } from "firebase-functions/lib/providers/firestore";
 import * as mailgun from "mailgun-js";
 import { Config, onlyWhenEnabled } from "./config";
 import { ensureCalledOnce } from "./ensure-called-once";
+import { DocumentReference } from "@google-cloud/firestore";
 
 const config = functions.config() as Config;
 const mailgunClient = mailgun({
@@ -24,28 +25,39 @@ export const enqueue = async (message: mailgun.messages.SendData) => {
   await db.collection(EMAILS_TO_SEND_COLLECTION_NAME).add({ message });
 };
 
+type AsyncProducer<T> = () => Promise<T>;
+
+const requeueIfFails = async <T>(
+  snapshot: DocumentSnapshot,
+  requeueRef: DocumentReference,
+  fn: AsyncProducer<T>
+): Promise<T> => {
+  try {
+    return fn();
+  } catch (err) {
+    console.warn(`requeuing ${snapshot.id} as ${requeueRef.id} after error:`, err);
+    await requeueRef.create({
+      ...snapshot.data()!,
+      retryOf: snapshot.ref,
+      retriedAt: firestore.Timestamp.now()
+    });
+    throw err;
+  }
+};
+
 const processEmail = async (emailSnapshot: DocumentSnapshot) => {
   const { id, ref, data } = emailSnapshot;
   const queuedEmail = data()! as QueuedEmail;
-  let sendResponse: mailgun.messages.SendResponse;
-  try {
-    sendResponse = await mailgunClient.messages().send({
-      ...queuedEmail.message,
-      to: config.features.emails.recipient_override || queuedEmail.message.to
-    });
-  } catch (err) {
-    const retryRef = db.collection(EMAILS_TO_SEND_COLLECTION_NAME).doc();
-    console.warn(`requeing ${id} as ${retryRef.id} after error:`, err);
-    // no try/catch here, loud fail
-    await retryRef.create({
-      ...queuedEmail,
-      retryOf: ref,
-      retriedAt: firestore.Timestamp.now()
-    });
-    return;
-  }
+  const sendResponse = await requeueIfFails(
+    emailSnapshot,
+    db.collection(EMAILS_TO_SEND_COLLECTION_NAME).doc(),
+    () =>
+      mailgunClient.messages().send({
+        ...queuedEmail.message,
+        to: config.features.emails.recipient_override || queuedEmail.message.to
+      })
+  );
   console.info("email sent: ", JSON.stringify(sendResponse));
-  // no try/catch here, loud fail
   await db
     .collection(EMAILS_SENT_COLLECTION_NAME)
     .doc(id)
