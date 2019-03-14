d909e6deb62aabd65d51a3866ec156b7cd852834
Clément Fassot <kedketh@gmail.com>
2019-03-14 17:01:09 +0100

    :white_check_mark: add checks for the production config (#82)
    These checks run before the deployment and verify that the prod config is sane.

---
+++ deployment/check-config/check-config.js
@@ -0,0 +1,55 @@
+const checkConfig = config => {
+  const computeStatisticsConfigs = config.features.compute_statistics;
+  const emailsConfig = config.features.emails;
+  const collectStatsConfig = config.features.collect_stats;
+  const importVotesConfig = config.features.import_votes;
+  const sendVoteToManagerConfig = config.features.send_vote_to_manager;
+  const remindersConfig = config.features.reminders;
+  const votingCampaignConfig = config.features.voting_campaigns;
+
+  const errors = ["Some errors occured:"];
+
+  if (computeStatisticsConfigs.enabled !== "false") {
+    errors.push("- compute_statistics is enabled");
+  }
+  if (emailsConfig.enabled !== "true") {
+    errors.push("- emails are disabled");
+  }
+  if (collectStatsConfig.enabled !== "true") {
+    errors.push("- collect_stats is disabled");
+  }
+  if (importVotesConfig.enabled !== "false") {
+    errors.push("- import_votes is enabled");
+  }
+  if (sendVoteToManagerConfig.enabled !== "true") {
+    errors.push("- send_vote_to_manager is disabled");
+  }
+  if (remindersConfig.voting_campaign_starts.enabled !== "true") {
+    errors.push("- reminders.voting_campaign_starts is disabled");
+  }
+  if (remindersConfig.voting_campaign_ends.enabled !== "true") {
+    errors.push("- reminders.voting_campaign_ends is disabled");
+  }
+  if (votingCampaignConfig.enabled !== "true") {
+    errors.push("- voting_campaign is disabled");
+  }
+  if (votingCampaignConfig.start_on !== "25") {
+    errors.push("- voting_campaign doesn't start on the 25th");
+  }
+  if (votingCampaignConfig.end_on !== "5") {
+    errors.push("- voting_campaign doesn't end on the 5th");
+  }
+
+  return errors;
+};
+
+if (require.main === module) {
+  const config = require(process.argv[2]);
+
+  const errors = checkConfig(config);
+
+  if (errors.length > 1) throw Error(errors.join("\n"));
+  else console.log("Config looks fine, continuing...");
+}
+
+exports.checkConfig = checkConfig;
