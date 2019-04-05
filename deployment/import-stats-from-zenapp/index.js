07b9c13151cd78e60f7adba0bb9bb5c4cf3defd9
Clément Fassot <kedketh@gmail.com>
2019-04-05 15:42:20 +0200

    :sparkles: add import script for zenapp aggregated stats (#86)

---
+++ deployment/import-stats-from-zenapp/index.js
@@ -0,0 +1,154 @@
+//@ts-check
+
+/**
+ * This script imports votes into the application given a JSON
+ * file. The JSON file must be in the format of an export from
+ * ZenApp, ie a JSON file created from a call to the HTTP API
+ * of Neo4j 2.3.
+ *
+ * Here is the HTTP request that we used to generate the JSON
+ * file.
+ * for global stats
+ * curl -i -X POST \
+ *  -H 'Content-Type:application/json' \
+ *  -d '{"statements":[{"statement":"MATCH (mood:MoodHistory)-->(month:Month)--(year:Year) RETURN mood, month, year"}]}' \
+ *  http://localhost:7474/db/data/transaction/commit
+ * for agencies stats
+ * curl -i -X POST \
+ *  -H 'Content-Type:application/json' \
+ *  -d '{"statements":[{"statement":"MATCH (mood:AgencyMoodHistory)-->(month:Month)--(year:Year), (mood)-->(agency:Agency) RETURN mood, agency, month, year"}]}' \
+ *  http://localhost:7474/db/data/transaction/commit
+ *
+ * The expected schema of the JSON file is documented as part
+ * of the ZenAppExportAgencies/ZenAppExportGlobal type defined in JSdoc below.
+ *
+ * The script converts the stats from the input file to the exported
+ * format of the import function then calls the import function with
+ * batches of stats until there are no more stats to send.
+ *
+ * The script expects 3 parameters: the 2 input files and the URI
+ * of the import function. The authorization key must be passed
+ * through the environment variable HUMEUR_IMPORT_AUTHORIZATION_KEY.
+ * The batch size can be customized using the environment variable
+ * HUMEUR_IMPORT_BATCH_SIZE. The default is 200.
+ */
+
+const fs = require("fs");
+const request = require("request-promise-native");
+
+/**
+ * Maps moods from values used by ZenApp
+ * to values used by Humeur du mois 2018
+ */
+
+const BATCH_SIZE = Number(process.env.HUMEUR_IMPORT_BATCH_SIZE) || 200;
+
+const AUTHORIZATION_KEY = process.env.HUMEUR_IMPORT_STATS_AUTHORIZATION_KEY;
+
+const [
+  zenAppExportGlobalDataFilePath,
+  zenAppExportAgencyDataFilePath,
+  importUri
+] = process.argv.slice(2);
+
+if (
+  !zenAppExportGlobalDataFilePath ||
+  !zenAppExportAgencyDataFilePath ||
+  !importUri
+) {
+  console.error(
+    "please provide the input file as the first argument and the import uri as the second"
+  );
+  process.exit(1);
+}
+
+/**
+ * @typedef {["mood", "month", "year"]} Columns
+ * @typedef {{ good: number, normal: number, bad: number, number: number }} MoodStats
+ * @typedef {{ value: number }} MonthValue
+ * @typedef {{ value: number }} YearValue
+ * @typedef {{ name: string }} AgencyValue
+ * @typedef {[MoodStats, MonthValue, YearValue]} RowGlobal
+ * @typedef {Array<{ row: RowGlobal }>} DataGlobal
+ * @typedef {{ results: [{ columns: Columns, data: DataGlobal }] }} ZenAppExportGlobal
+ * @type {ZenAppExportGlobal}
+ */
+const zenAppExportGlobalData = JSON.parse(
+  fs.readFileSync(zenAppExportGlobalDataFilePath).toString()
+);
+/**
+ * @typedef {[MoodStats, AgencyValue, MonthValue, YearValue]} RowAgencies
+ * @typedef {Array<{ row: RowAgencies }>} DataAgencies
+ * @typedef {{ results: [{ columns: Columns, data: DataAgencies }] }} ZenAppExportAgencies
+ * @type {ZenAppExportAgencies}
+ */
+const zenAppExportAgencyData = JSON.parse(
+  fs.readFileSync(zenAppExportAgencyDataFilePath).toString()
+);
+
+/**
+ * @typedef {{great: number, notThatGreat: number, notGreatAtAll: number}} MoodExport
+ * @type {{mood: MoodExport, campaign: string}[]}
+ */
+const globalStats = zenAppExportGlobalData.results[0].data.map(
+  ({
+    row: [{ good, normal, bad, number }, { value: month }, { value: year }]
+  }) => ({
+    mood: {
+      great: Math.floor((number * good) / 100),
+      notThatGreat: Math.floor((number * normal) / 100),
+      notGreatAtAll: Math.floor((number * bad) / 100)
+    },
+    campaign: new Date(Date.UTC(year, month - 1)).toISOString().substr(0, 7)
+  })
+);
+
+/**
+ * @type {{mood: MoodExport, agency: string, campaign: string}[]}
+ */
+const agencyStats = zenAppExportAgencyData.results[0].data.map(
+  ({
+    row: [
+      { good, normal, bad, number },
+      { name: agency },
+      { value: month },
+      { value: year }
+    ]
+  }) => ({
+    mood: {
+      great: Math.floor((number * good) / 100),
+      notThatGreat: Math.floor((number * normal) / 100),
+      notGreatAtAll: Math.floor((number * bad) / 100)
+    },
+    agency,
+    campaign: new Date(Date.UTC(year, month - 1)).toISOString().substr(0, 7)
+  })
+);
+
+sendData(globalStats);
+sendData(agencyStats);
+
+/**
+ * @param {{mood: MoodExport, campaign: string}[]} stats
+ */
+async function sendData(stats) {
+  const leftToSend = [...stats];
+  while (leftToSend.length > 0) {
+    const nextBatch = leftToSend.splice(0, BATCH_SIZE);
+    console.info(
+      `sending batch of ${nextBatch.length} entries, ${
+        leftToSend.length
+      } entries left`
+    );
+    const response = await request({
+      method: "POST",
+      uri: importUri,
+      body: { stats: nextBatch },
+      json: true,
+      headers: {
+        Authorization: `Bearer ${AUTHORIZATION_KEY}`
+      }
+    });
+    console.log(`got response ${response}`);
+  }
+}
---
+++ functions/src/import-stats.ts
@@ -0,0 +1,143 @@
+import * as functions from "firebase-functions";
+import * as firebase from "firebase-admin";
+
+import { Config } from "./config";
+
+type Statistic = {
+  mood: {
+    great?: number;
+    notThatGreat?: number;
+    notGreatAtAll?: number;
+  };
+  campaign: string;
+  agency?: string;
+};
+type StatisticToInsert = {
+  great?: number;
+  notThatGreat?: number;
+  notGreatAtAll?: number;
+  campaign: string;
+  agency?: string;
+};
+
+const config = functions.config() as Config;
+const importStatsConfigs = config.features.import_stats;
+const db = firebase.firestore();
+
+const buildAgencyStatsBatch = (
+  validStats: StatisticToInsert[]
+): FirebaseFirestore.WriteBatch => {
+  const statsBatch = db.batch();
+  validStats.forEach(validStat => {
+    statsBatch.set(
+      db
+        .collection("stats-campaign-agency")
+        .doc(`${validStat.campaign}_${validStat.agency}`),
+      validStat
+    );
+  });
+  return statsBatch;
+};
+
+const buildGlobalStatsBatch = (
+  validStats: StatisticToInsert[]
+): FirebaseFirestore.WriteBatch => {
+  const statsBatch = db.batch();
+  validStats.forEach((validStat, _) => {
+    statsBatch.set(
+      db.collection("stats-campaign").doc(`${validStat.campaign}`),
+      validStat
+    );
+  });
+  return statsBatch;
+};
+
+const convertValidStatsToDbObject = ({
+  mood,
+  agency,
+  campaign
+}: Statistic): StatisticToInsert => {
+  return {
+    ...mood,
+    ...(agency && { agency }),
+    campaign: campaign
+  };
+};
+
+const fromJSONtoStats = (
+  supposedlyValidStats: any
+): [StatisticToInsert[], boolean] => {
+  const validStats = new Map<string, StatisticToInsert>();
+  let isAgencyStats = false;
+  if (!supposedlyValidStats.stats) {
+    return [[...validStats.values()], isAgencyStats];
+  }
+  supposedlyValidStats.stats
+    .filter(
+      (supposedlyValidStat: any) =>
+        supposedlyValidStat.mood &&
+        typeof supposedlyValidStat.mood === "object" &&
+        supposedlyValidStat.campaign
+    )
+    .forEach((validStat: Statistic) => {
+      if (validStat.agency) {
+        isAgencyStats = true;
+        validStats.set(
+          `${validStat.campaign}_${validStat.agency}`,
+          convertValidStatsToDbObject(validStat)
+        );
+      } else {
+        validStats.set(
+          `${validStat.campaign}`,
+          convertValidStatsToDbObject(validStat)
+        );
+      }
+    });
+  console.info(
+    `ignored ${supposedlyValidStats.stats.length -
+      validStats.size} votes because they did not match the expected schema`
+  );
+  return [[...validStats.values()], isAgencyStats];
+};
+
+export const importStats = functions.https.onRequest(
+  async (req: functions.Request, res: functions.Response) => {
+    if (!importStatsConfigs.enabled) {
+      console.info("Feature import stats disabled, aborting");
+      return;
+    }
+    const authorizationHeader = req.get("Authorization") || "";
+    const keyIsCorrect =
+      authorizationHeader === `Bearer ${importStatsConfigs.key}`;
+    if (!keyIsCorrect) {
+      console.error("Passed the wrong auth key, aborting");
+      res.sendStatus(403);
+      return;
+    }
+    if (!req.body) {
+      console.error("statsData is null, aborting");
+      res.sendStatus(422);
+      return;
+    }
+    const [validStats, isAgencyStats] = fromJSONtoStats(req.body);
+
+    /* Security mesure, to make sure we don't miss any of thoose. But it should really be called with
+     * only agencies stats or only global stats
+     */
+    if (validStats.length <= 0) {
+      console.error("statsData is empty, aborting");
+      res.sendStatus(422);
+      return;
+    }
+    const statsBatch = isAgencyStats
+      ? buildAgencyStatsBatch(validStats)
+      : buildGlobalStatsBatch(validStats);
+    statsBatch
+      .commit()
+      .then(() => res.sendStatus(200))
+      .catch(e => {
+        console.error(e);
+        res.sendStatus(500);
+      });
+  }
+);
