868b1f133be12fab9648944b99348129a7f8bf4e
clement <clement.vanpeuter@zenika.com>
2019-05-28 10:43:00 +0200

    change the compose email html function and add two tests

--- functions/src/compose-email-html.ts
+++ functions/src/compose-email-html.ts
@@ -6,22 +6,25 @@ const voteMap: { [key: string]: string } = {
 };
 
 const composeEmailHtml = (vote: Vote) => {
+  if (vote.comment) {
+    return `
+    <p>Hi ${vote.managerEmail},</p>
+    <p>
+      ${vote.fullName} has shared how they feel:
+      "${voteMap[vote.value]}".
+    </p>
+    <p>and left a comment:
+    ${vote.comment}
+    </p>
+    <p>See you soon!</p>`;
+  }
   return `
-      <p>Hi ${vote.managerEmail},</p>
-      <p>
-        ${vote.fullName} has shared how they feel:
-        "${voteMap[vote.value]}".
-      </p>
-      ${
-        vote.comment
-          ? `
-      <p>
-        And left this comment:
-        "${vote.comment}".
-      </p>`
-          : ``
-      }
-      <p>See you soon!</p>`;
+    <p>Hi ${vote.managerEmail},</p>
+    <p>
+      ${vote.fullName} has shared how they feel:
+      "${voteMap[vote.value]}".
+    </p>
+    <p>See you soon!</p>`;
 };
 
 export default composeEmailHtml;
