4e3ad7a25e2c500fd84f5d4a6d20fb5908c413fb
Hugo Wood <hugo.wood@zenika.com>
2021-08-26 17:12:07 +0200

    use firestore ids as tokens

--- functions/src/generate-random-email-token.ts
+++ functions/src/generate-random-email-token.ts
@@ -1,4 +1,3 @@
-import { randomBytes } from "crypto";
 import { firestore } from "firebase-admin";
 
 export async function generateAndSaveRandomEmailToken(
@@ -6,22 +5,8 @@ export async function generateAndSaveRandomEmailToken(
   campaignId: string,
   db: firestore.Firestore
 ): Promise<string> {
-  const token = generateRandomEmailToken();
-  await db
+  const ref = await db
     .collection("token")
-    .doc(token)
-    .create({ employeeEmail: employeeEmail, campaignId: campaignId });
-  return token;
-}
-
-function generateRandomEmailToken() {
-  return encodeAsBase64Url(randomBytes(64));
-}
-
-function encodeAsBase64Url(buffer: Buffer): string {
-  return buffer
-    .toString("base64")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=*$/, "");
+    .add({ employeeEmail: employeeEmail, campaignId: campaignId });
+  return ref.id
 }
