6c36831a92979e738455fd867c50bfd460e7d54e
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2021-09-07 14:42:18 +0200

    Correction config sender name

--- functions/src/compose-email-sender.ts
+++ functions/src/compose-email-sender.ts
@@ -4,7 +4,7 @@ import { Config, isEnabled, asNumber } from "./config";
 const config = functions.config() as Config;
 
 export function composeEmailSender() {
-  return `${config.features.emails.name || "Humeur du mois"} <${
-    config.features.emails.email || "humeur-du-mois@zenika.com"
+  return `${config.features.emails.sender.name || "Humeur du mois"} <${
+    config.features.emails.sender.email || "humeur-du-mois@zenika.com"
   }>`;
 }
