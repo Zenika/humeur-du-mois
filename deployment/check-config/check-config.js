9ad55b2945a395472fe3ece3a71092f2b9376202
Hugo Wood <hugo.wood@zenika.com>
2021-10-28 12:00:45 +0200

    🛠 improve check prod config script (#199)

--- deployment/check-config/check-config.js
+++ deployment/check-config/check-config.js
@@ -1,50 +1,67 @@
+//@ts-check
+
+/**
+ * Checks that the config is appropriate for production.
+ *
+ * @param {import("../../functions/src/config").Config} config
+ */
 const checkConfig = config => {
-  const computeStatisticsConfigs = config.features.compute_statistics;
-  const emailsConfig = config.features.emails;
-  const collectStatsConfig = config.features.collect_stats;
-  const importVotesConfig = config.features.import_votes;
-  const sendVoteToManagerConfig = config.features.send_vote_to_manager;
-  const remindersConfig = config.features.reminders;
-  const votingCampaignConfig = config.features.voting_campaigns;
-  const allowManualTriggerOfCampaignStartReminder =
-    config.features.allow_force_send_campaign_reminder === "true";
+  const emailsConfig = config.features?.emails;
+  const collectStatsConfig = config.features?.collect_stats;
+  const computeStatisticsConfigs = config.features?.compute_statistics;
+  const importStatsConfig = config.features?.import_stats;
+  const importVotesConfig = config.features?.import_votes;
+  const backup = config.features?.backup;
+  const sendVoteToManagerConfig = config.features?.send_vote_to_manager;
+  const remindersConfig = config.features?.reminders;
+  const votingCampaignConfig = config.features?.voting_campaigns;
 
-  const errors = ["Some errors occured:"];
+  const errors = [];
 
-  if (computeStatisticsConfigs.enabled !== "false") {
-    errors.push("- compute_statistics is enabled");
-  }
-  if (emailsConfig.enabled !== "true") {
+  if (emailsConfig?.enabled !== "true") {
     errors.push("- emails are disabled");
   }
-  if (collectStatsConfig.enabled !== "true") {
+  if (collectStatsConfig?.enabled !== "true") {
     errors.push("- collect_stats is disabled");
   }
-  if (importVotesConfig.enabled !== "false") {
+  if (computeStatisticsConfigs?.enabled === "true") {
+    errors.push("- compute_statistics is enabled");
+  }
+  if (importStatsConfig?.enabled === "true") {
+    errors.push("- import_stats is enabled");
+  }
+  if (importVotesConfig?.enabled === "true") {
     errors.push("- import_votes is enabled");
   }
-  if (sendVoteToManagerConfig.enabled !== "true") {
+  if (backup?.enabled === "true" && !backup?.key) {
+    errors.push("- backup is enabled but no key is defined");
+  }
+  if (
+    remindersConfig?.voting_campaign_starts?.force?.enabled === "true" &&
+    !remindersConfig?.voting_campaign_starts?.force?.key
+  ) {
+    errors.push(
+      "- reminders.voting_campaign_starts.force is enabled but no key is defined"
+    );
+  }
+  if (sendVoteToManagerConfig?.enabled !== "true") {
     errors.push("- send_vote_to_manager is disabled");
   }
-  if (remindersConfig.voting_campaign_starts.enabled !== "true") {
+  if (remindersConfig?.voting_campaign_starts?.enabled !== "true") {
     errors.push("- reminders.voting_campaign_starts is disabled");
   }
-  if (remindersConfig.voting_campaign_ends.enabled !== "true") {
+  if (remindersConfig?.voting_campaign_ends.enabled !== "true") {
     errors.push("- reminders.voting_campaign_ends is disabled");
   }
-  if (votingCampaignConfig.enabled !== "true") {
+  if (votingCampaignConfig?.enabled !== "true") {
     errors.push("- voting_campaign is disabled");
   }
-  if (votingCampaignConfig.start_on !== "25") {
+  if (votingCampaignConfig?.start_on !== "25") {
     errors.push("- voting_campaign doesn't start on the 25th");
   }
-  if (votingCampaignConfig.end_on !== "5") {
+  if (votingCampaignConfig?.end_on !== "5") {
     errors.push("- voting_campaign doesn't end on the 5th");
   }
-  if (allowManualTriggerOfCampaignStartReminder) {
-    errors.push("- function to directly send vote email is enabled");
-  }
-
   return errors;
 };
 
@@ -53,8 +70,11 @@ if (require.main === module) {
 
   const errors = checkConfig(config);
 
-  if (errors.length > 1) throw Error(errors.join("\n"));
-  else console.log("Config looks fine, continuing...");
+  if (errors.length > 0) {
+    throw Error(["Some errors occured:", ...errors].join("\n"));
+  } else {
+    console.log("Config looks fine, continuing...");
+  }
 }
 
 exports.checkConfig = checkConfig;
--- deployment/check-config/check-config.test.js
+++ deployment/check-config/check-config.test.js
@@ -31,14 +31,12 @@ test("Config is suitable for production", t => {
         enabled: "true",
         start_on: "25",
         end_on: "5"
-      },
-      allow_force_send_campaign_reminder: "false"
+      }
     }
   };
   const errors = checkConfig(config);
 
-  if (errors.length > 1) t.fail();
-  else t.pass();
+  t.deepEqual(errors, []);
 });
 
 test("Config is not suitable for production", t => {
@@ -53,12 +51,22 @@ test("Config is not suitable for production", t => {
       import_votes: {
         enabled: "true"
       },
+      import_stats: {
+        enabled: "true"
+      },
+      backup: {
+        enabled: "true"
+      },
       send_vote_to_manager: {
         enabled: "false"
       },
       reminders: {
         voting_campaign_ends: {
-          enabled: "false"
+          enabled: "false",
+          force: {
+            enabled: "true",
+            key: ""
+          }
         },
         voting_campaign_starts: {
           enabled: "false"
@@ -71,12 +79,23 @@ test("Config is not suitable for production", t => {
         enabled: "false",
         start_on: "2",
         end_on: "6"
-      },
-      allow_force_send_campaign_reminder: "true"
+      }
     }
   };
   const errors = checkConfig(config);
 
-  if (errors.length < 11) t.fail();
-  else t.pass();
+  t.deepEqual(errors, [
+    "- emails are disabled",
+    "- collect_stats is disabled",
+    "- compute_statistics is enabled",
+    "- import_stats is enabled",
+    "- import_votes is enabled",
+    "- backup is enabled but no key is defined",
+    "- send_vote_to_manager is disabled",
+    "- reminders.voting_campaign_starts is disabled",
+    "- reminders.voting_campaign_ends is disabled",
+    "- voting_campaign is disabled",
+    "- voting_campaign doesn't start on the 25th",
+    "- voting_campaign doesn't end on the 5th"
+  ]);
 });
