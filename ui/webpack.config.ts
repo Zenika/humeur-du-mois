9e28a637d90fee7a023c9a55f5ed8af917db55de
Hugo Wood <hugo.wood@zenika.com>
2021-11-09 14:37:38 +0100

    🚑 fix production firebase config

--- ui/webpack.config.ts
+++ ui/webpack.config.ts
@@ -27,22 +27,26 @@ export default (): webpack.Configuration => ({
     }),
     new webpack.DefinePlugin({
       __ENV__: {
-        AUTH0_CONFIG: JSON.stringify({
-          domain: "zenika.eu.auth0.com",
-          client_id: "j5xIoOh3R9Jov6wtKQm2BAHUSkrYpttY",
-          audience: "https://zenika.eu.auth0.com/userinfo",
-          scope: "openid email",
-          connection: "google-oauth2"
-        }),
-        FIREBASE_CONFIG: JSON.stringify({
-          apiKey: "AIzaSyDEcSPU1xW0ReLEvEQoiTH-a003XFsAueQ",
-          authDomain: "humeur-du-mois-2018.firebaseapp.com",
-          databaseURL: "https://humeur-du-mois-2018.firebaseio.com",
-          projectId: "humeur-du-mois-2018",
-          storageBucket: "humeur-du-mois-2018.appspot.com",
-          messagingSenderId: "613593816673",
-          appId: "1:613593816673:web:908fcded3c60516fef5420"
-        })
+        AUTH0_CONFIG:
+          process.env.HUMEUR_DU_MOIS_UI_AUTH0_CONFIG ||
+          JSON.stringify({
+            domain: "zenika.eu.auth0.com",
+            client_id: "j5xIoOh3R9Jov6wtKQm2BAHUSkrYpttY",
+            audience: "https://zenika.eu.auth0.com/userinfo",
+            scope: "openid email",
+            connection: "google-oauth2"
+          }),
+        FIREBASE_CONFIG:
+          process.env.HUMEUR_DU_MOIS_UI_FIREBASE_CONFIG ||
+          JSON.stringify({
+            apiKey: "AIzaSyDEcSPU1xW0ReLEvEQoiTH-a003XFsAueQ",
+            authDomain: "humeur-du-mois-2018.firebaseapp.com",
+            databaseURL: "https://humeur-du-mois-2018.firebaseio.com",
+            projectId: "humeur-du-mois-2018",
+            storageBucket: "humeur-du-mois-2018.appspot.com",
+            messagingSenderId: "613593816673",
+            appId: "1:613593816673:web:908fcded3c60516fef5420"
+          })
       }
     })
   ]
