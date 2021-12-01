12aef08c01148c863a58373b521da3559d260c9a
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2021-12-01 12:34:59 +0100

    Correct build

--- ui/src/services/computeDataFromDataBase.ts
+++ ui/src/services/computeDataFromDataBase.ts
@@ -6,10 +6,6 @@ export function computeDataFromDataBase(voteRawData: VoteData) {
   voteRawData = voteRawData.map(row => ({
     ...row,
     counts: {
-      great: 0,
-      notGreatAtAll: 0,
-      notThatGreat: 0,
-      ok: 0,
       ...row.counts
     }
   }));
